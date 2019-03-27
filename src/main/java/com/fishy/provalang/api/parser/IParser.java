79ff73cea2f54083ea01025c2210fbb67c73d882
BSFishy <mttprvstanddad@gmail.com>
2019-03-27 11:35:53 +0000

    Remove old parsing api package

--- src/main/java/com/fishy/provalang/api/parser/IParser.java
+++
@@ -1,8 +0,0 @@
-package com.fishy.provalang.api.parser;
-
-import com.fishy.provalang.file.Program;
-
-public interface IParser
-{
-    void parse(Program p); // TODO: work with lexer stream not list
-}
