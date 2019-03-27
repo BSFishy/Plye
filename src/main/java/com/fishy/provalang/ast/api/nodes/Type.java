d79eb9b7b13a4c6071943f57370c49c7b836801a
BSFishy <mttprvstanddad@gmail.com>
2019-03-27 09:38:28 +0000

    Delete old AST

--- src/main/java/com/fishy/provalang/ast/api/nodes/Type.java
+++
@@ -1,6 +0,0 @@
-package com.fishy.provalang.ast.api.nodes;
-
-public interface Type
-{
-    String getName();
-}
--- src/main/java/com/fishy/provalang/ast/api/scope/scopes/RootScope.java
+++
@@ -1,9 +0,0 @@
-package com.fishy.provalang.ast.api.scope.scopes;
-
-public class RootScope extends GenericScope
-{
-    public RootScope()
-    {
-        super(null);
-    }
-}
