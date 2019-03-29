1f6c5e282f4cc17f7996564634e233d67a4af876
BSFishy <mttprvstanddad@gmail.com>
2019-03-29 10:36:10 +0000

    Add parser pass tokens

--- src/main/java/com/fishy/provalang/api/data/NodeHandlerData.java
+++
@@ -1,9 +0,0 @@
-package com.fishy.provalang.api.data;
-
-import lombok.Data;
-
-@Data
-public class NodeHandlerData
-{
-    public final boolean handle;
-}
---
+++ src/main/java/com/fishy/provalang/api/parser/pass/TokenConverter.java
@@ -0,0 +1,5 @@
+package com.fishy.provalang.api.parser.pass;
+
+public abstract class TokenConverter<T extends PassToken, K extends PassToken>
+{
+}
