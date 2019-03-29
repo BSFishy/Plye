6a6c1434276124865c3a900a625b054cb57fad58
BSFishy <mttprvstanddad@gmail.com>
2019-03-29 09:09:51 +0000

    Preparation for implementation Prepare some stuff for the new design for the parser.

--- src/main/java/com/fishy/provalang/api/parser/ParserPass.java
+++ src/main/java/com/fishy/provalang/api/parser/ParserPass.java
@@ -1,21 +1,5 @@
 package com.fishy.provalang.api.parser;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public abstract class ParserPass
 {
-    private final List<NodeHandler> handlers = new ArrayList<>();
-
-    public abstract void addDefaultHandlers();
-
-    public void addHandler(NodeHandler handler)
-    {
-        handlers.add(handler);
-    }
-
-    public List<NodeHandler> getHandlers()
-    {
-        return handlers;
-    }
 }
