2e47b492b1d99daed3c2338098c64d8c908a1198
BSFishy <mttprvstanddad@gmail.com>
2019-04-02 09:54:25 +0000

    Add method definitions to Ast Api

--- src/main/java/com/fishy/provalang/api/ast/AstApi.java
+++ src/main/java/com/fishy/provalang/api/ast/AstApi.java
@@ -1,6 +1,117 @@
 package com.fishy.provalang.api.ast;
 
+import com.fishy.provalang.api.ast.feature.FeatureNode;
+import com.fishy.provalang.api.ast.type.AstBlock;
+import com.fishy.provalang.api.ast.type.AstStatement;
+
 public class AstApi
 {
+    public static boolean isNode(AstElement element) {
+        return false;
+    }
 
+    public static boolean isFeature(AstElement element)
+    {
+        return false;
+    }
+
+    public static boolean isBlock(AstElement element)
+    {
+        return false;
+    }
+
+    public static boolean isBlock(AstNode node)
+    {
+        return false;
+    }
+
+    public static boolean isBlock(FeatureNode node)
+    {
+        return false;
+    }
+
+    public static boolean isStatement(AstElement element)
+    {
+        return false;
+    }
+
+    public static boolean isStatement(AstNode node)
+    {
+        return false;
+    }
+
+    public static boolean isFeatureNode(AstElement element)
+    {
+        return false;
+    }
+
+    public static boolean isFeatureNode(AstNode node)
+    {
+        return false;
+    }
+
+    public static boolean isBlockNode(AstElement element)
+    {
+        return false;
+    }
+
+    public static boolean isBlockNode(AstNode node)
+    {
+        return false;
+    }
+
+    public static boolean isBlockNode(AstStatement statement)
+    {
+        return false;
+    }
+
+    public static boolean isBlockNode(AstBlock block)
+    {
+        return false;
+    }
+
+    public static boolean isBlockNode(FeatureNode node)
+    {
+        return false;
+    }
+
+    public static boolean isContainerNode(AstElement element)
+    {
+        return false;
+    }
+
+    public static boolean isContainerNode(AstNode node)
+    {
+        return false;
+    }
+
+    public static boolean isContainerNode(AstBlock block)
+    {
+        return false;
+    }
+
+    public static boolean isContainerNode(FeatureNode node)
+    {
+        return false;
+    }
+
+    public static boolean isExpressionNode(AstElement element)
+    {
+        return false;
+    }
+
+    public static boolean isExpressionNode(AstNode node)
+    {
+        return false;
+    }
+
+    public static boolean isExpressionNode(FeatureNode node)
+    {
+        return false;
+    }
+
+    public static boolean isExpressionNode(AstStatement statement)
+    {
+        return false;
+    }
 }
