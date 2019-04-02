2830d9231b031fcf707687c6a632ebd87c0cd0b1
BSFishy <mttprvstanddad@gmail.com>
2019-04-02 09:58:27 +0000

    Add Ast Api implementations

--- src/main/java/com/fishy/provalang/api/ast/AstApi.java
+++ src/main/java/com/fishy/provalang/api/ast/AstApi.java
@@ -1,117 +1,119 @@
 package com.fishy.provalang.api.ast;
 
+import com.fishy.provalang.api.ast.feature.Feature;
 import com.fishy.provalang.api.ast.feature.FeatureNode;
-import com.fishy.provalang.api.ast.type.AstBlock;
-import com.fishy.provalang.api.ast.type.AstStatement;
+import com.fishy.provalang.api.ast.type.*;
 
+@SuppressWarnings("unused")
 public class AstApi
 {
-    public static boolean isNode(AstElement element) {
-        return false;
+    public static boolean isNode(AstElement element)
+    {
+        return element instanceof AstNode;
     }
 
     public static boolean isFeature(AstElement element)
     {
-        return false;
+        return element instanceof Feature;
     }
 
     public static boolean isBlock(AstElement element)
     {
-        return false;
+        return element instanceof AstBlock;
     }
 
     public static boolean isBlock(AstNode node)
     {
-        return false;
+        return node instanceof AstBlock;
     }
 
     public static boolean isBlock(FeatureNode node)
     {
-        return false;
+        return node instanceof AstBlock;
     }
 
     public static boolean isStatement(AstElement element)
     {
-        return false;
+        return element instanceof AstStatement;
     }
 
     public static boolean isStatement(AstNode node)
     {
-        return false;
+        return node instanceof AstStatement;
     }
 
     public static boolean isFeatureNode(AstElement element)
     {
-        return false;
+        return element instanceof FeatureNode;
     }
 
     public static boolean isFeatureNode(AstNode node)
     {
-        return false;
+        return node instanceof FeatureNode;
     }
 
     public static boolean isBlockNode(AstElement element)
     {
-        return false;
+        return element instanceof BlockNode;
     }
 
     public static boolean isBlockNode(AstNode node)
     {
-        return false;
+        return node instanceof BlockNode;
     }
 
     public static boolean isBlockNode(AstStatement statement)
     {
-        return false;
+        return statement instanceof BlockNode;
     }
 
     public static boolean isBlockNode(AstBlock block)
     {
-        return false;
+        return block instanceof BlockNode;
     }
 
     public static boolean isBlockNode(FeatureNode node)
     {
-        return false;
+        return node instanceof BlockNode;
     }
 
     public static boolean isContainerNode(AstElement element)
     {
-        return false;
+        return element instanceof ContainerNode;
     }
 
     public static boolean isContainerNode(AstNode node)
     {
-        return false;
+        return node instanceof ContainerNode;
     }
 
     public static boolean isContainerNode(AstBlock block)
     {
-        return false;
+        return block instanceof ContainerNode;
     }
 
     public static boolean isContainerNode(FeatureNode node)
     {
-        return false;
+        return node instanceof ContainerNode;
     }
 
     public static boolean isExpressionNode(AstElement element)
     {
-        return false;
+        return element instanceof ExpressionNode;
     }
 
     public static boolean isExpressionNode(AstNode node)
     {
-        return false;
+        return node instanceof ExpressionNode;
     }
 
     public static boolean isExpressionNode(FeatureNode node)
     {
-        return false;
+        return node instanceof ExpressionNode;
     }
 
     public static boolean isExpressionNode(AstStatement statement)
     {
-        return false;
+        return statement instanceof ExpressionNode;
     }
 }
